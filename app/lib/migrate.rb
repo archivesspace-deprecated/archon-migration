5ec3d0a6452096e003019fc63f343cd38fe817b9
Brian Hoffman <brianjhoffman@gmail.com>
2013-08-05 16:09:09 -0400

    move record mapping logic to model definitions

--- app/lib/migrate.rb
+++ app/lib/migrate.rb
@@ -45,55 +45,13 @@ class MigrationJob
     #open up the batch file
     @archivesspace.import(y) do |batch|
       
-      Archon.record_type(:subject).each do |id, subject|
-
-        $log.debug("Migrating Record: #{id}")
+      Archon.record_type(:subject).each do |subject|
         $log.debug(subject)
 
-        if %w(3 8 10).include?(subject["SubjectTypeID"])
-          # build an agent
-        else
-          # build a subject
+        aspace_subject = subject.class.transform(subject)
 
-          terms = build_terms(subject)
-
-          # ASpaceImport.JSONModel(:subject).from_hash({
-          #                                              #:external_ids => [{:external_id => subject["ID"]}],
-          #                                              :terms => terms
-          #                                             })
-
-          s = JSONModel.JSONModel(:subject).new
-          s.uri = s.class.uri_for(subject["ID"])
-          s.terms = terms
-          s.external_ids = [{:external_id => subject["ID"], :source => "Archon"}]
-          s.vocabulary = '/vocabularies/1'
-          batch << s
-        end
+        batch << aspace_subject
       end
-    end
-  end
-
-
-  def build_terms(subject, terms = [])
-    if subject["Parent"]
-      terms = build_terms(subject["Parent"], terms)
-    end
-
-    terms << {:term => subject["Subject"], :term_type => term_type(subject["SubjectTypeID"]), :vocabulary => '/vocabularies/1'}
-
-    terms
-  end
-
-
-  def term_type(archon_subject_type_id)
-    case archon_subject_type_id
-    when '4'; 'function'
-    when '5'; 'genre_form'
-    when '6'; 'geographic'
-    when '7'; 'occupation'
-    when '2'; 'temporal'
-    when '1'; 'topical'
-    when '9'; 'uniform_title'
     end
   end
 end
--- app/models/archon_records.rb
+++ app/models/archon_records.rb
@@ -1,9 +1,79 @@
 
-Archon.record_type(:subject) do
-  
-  @p = 'core/subjects'
-
+Archon.record_type(:repository) do
+  plural 'repositories'
 end
 
 
+Archon.record_type(:user) do
+  plural 'users'
+end
 
+
+Archon.record_type(:subjectsource) do
+  plural 'subjectsources'
+  include  Archon::EnumRecord
+end
+
+
+Archon.record_type(:subject) do
+  self.plural 'subjects'
+
+  def self.transform(rec)
+    # build an agent
+    if %w(3 8 10).include?(rec["SubjectTypeID"])
+      return
+    # build a subject
+    else
+      terms = build_terms(rec)
+      source = Archon.record_type(:subjectsource).find(rec["SubjectSourceID"])
+
+      obj = ASpaceImport.JSONModel(:subject).new
+      obj.class.uri_for(rec["ID"])
+      obj.terms = terms
+      obj.external_ids = [{:external_id => rec["ID"], :source => "Archon"}]
+      obj.vocabulary = '/vocabularies/1'
+      obj.source = source["EADSource"]
+    end
+
+    obj
+  end
+
+
+  def self.build_terms(rec, terms = [])
+    if rec["Parent"]
+      terms = build_terms(rec["Parent"], terms)
+    end
+
+    terms << {:term => rec["Subject"], :term_type => term_type(rec["SubjectTypeID"]), :vocabulary => '/vocabularies/1'}
+
+    terms
+  end
+
+
+  def self.term_type(archon_subject_type_id)
+    case archon_subject_type_id
+    when '4'; 'function'
+    when '5'; 'genre_form'
+    when '6'; 'geographic'
+    when '7'; 'occupation'
+    when '2'; 'temporal'
+    when '1'; 'topical'
+    when '9'; 'uniform_title'
+    end
+  end
+end
+
+# ?p=core/creators&batch_start=1
+
+# /?p=core/classifications&batch_start=1
+
+
+# ?p=core/collections&batch_start=1
+# ?p=core/accessions&batch_start=1
+# ?p=core/content&cid=integer&batch_start=1
+
+
+
+
+# ?p=core/digitalcontent&batch_start=1
+# ?p=core/digitalfiles&batch_start=1
