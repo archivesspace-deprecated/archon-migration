b716d1b7f70872698f88ca5ad0b6c2537dc68fde
Brian Hoffman <brianjhoffman@gmail.com>
2013-08-19 11:47:59 -0400

    methods to support integrated tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,43 @@ require 'rspec'
 require_relative '../app/lib/startup'
 require_relative '../app/lib/archon_client'
 require_relative '../app/lib/archivesspace_client'
+require_relative '../app/lib/migrate'
 
 
 def get_archon_client
   begin
-    Archon::Client.new(
-                       :url => Appdata.default_archon_url,
-                       :user => Appdata.default_archon_user,
-                       :password => 'admin'
-                       )
+    Archon::Client.new
   rescue Archon::ArchonAuthenticationError
     nil
+  end
+end
+
+
+def get_aspace_client
+  ArchivesSpace::Client.new
+end
+
+
+def nuke_aspace(client=nil)
+  client = client ? client : get_aspace_client
+  
+  url = URI("#{Appdata.default_aspace_url}/")
+  req = Net::HTTP::Delete.new(url.request_uri)
+
+  response = JSONModel::HTTP.do_http_request(url, req)
+  Thread.current[:backend_session] = nil
+end
+
+
+def verify_archon_dataset(client=nil)
+  client = client ? client : get_archon_client
+  if client
+    repo = client.get_json('/?p=core/repositories&batch_start=1')
+    unless repo['1']['Name'] == "Archon Migration Tracer"
+      pending "an Archon instance running against the archon_tracer database"
+    end
+  else
+    pending "an instance of Archon to run the test against"
   end
 end
 
