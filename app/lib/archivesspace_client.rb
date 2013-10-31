97f36fe4b95e6ca17e41f6318a9c06c8ea0ff592
Brian Hoffman <brianjhoffman@gmail.com>
2013-10-30 23:39:24 -0400

    attempt to make the archivesspace client more resilient to brief network interruptions

--- app/lib/archivesspace_client.rb
+++ app/lib/archivesspace_client.rb
@@ -36,8 +36,10 @@ module ArchivesSpace
 
   module HTTP
 
-    def init_session
-      $log.debug("Logging into ArchivesSpace")
+    def init_session(triesleft = 20)
+      $log.debug("Attempt logging into ArchivesSpace")
+      Thread.current[:backend_session] = nil
+
       url = URI("#{@url}/users/#{@user}/login")
       raise URIException, "URI format error: #{@url}" unless URI::HTTP === url
 
@@ -46,7 +48,13 @@ module ArchivesSpace
 
       response = JSONModel::HTTP.do_http_request(url, req)
       unless response.code == '200'
-        raise "Couldn't log into ArchivesSpace and start a session"
+        if triesleft > 0
+          $log.debug("Log in failed: try again in 1 second")
+          sleep(1)
+          init_session(triesleft - 1)
+        else
+          raise "Giving up: couldn't log into ArchivesSpace and start a session"
+        end
       end
 
       json = JSON::parse(response.body)
@@ -142,12 +150,15 @@ module ArchivesSpace
 
       # if cache.empty? && seen_records.empty?
       if cache.empty? && working_file.size == 0
-        $log.warn("Empty batch: aborting, not saving")
+        $log.warn("Empty batch: not saving")
         return {} 
       end
 
       # save the batch
       $log.debug("Posting import batch")
+
+      init_session # log in before posting a batch
+
       cache.save! do |response|
         if response.code.to_s == '200'
 
@@ -195,7 +206,7 @@ module ArchivesSpace
         end
       elsif message['saved'] && message['saved'].is_a?(Hash)
         r = {
-          :type => 'status',
+          :type => 'update',
           :source => 'aspace',
           :body => "Saved #{message['saved'].keys.count} records"
         }
@@ -204,16 +215,16 @@ module ArchivesSpace
       elsif message['status'].respond_to?(:length)
         message['status'].each do |status|
           if status['type'] == 'started'
-            r =  {
-              :type => 'status',
+            r =  {            
               :source => 'aspace',
               :body => status['label'],
               :id => status['id']
             }
+            r[:type] = r[:body] =~ /^Saved/ ? :update : :flash
             yield r
           elsif status['type'] == 'refresh'
             r = {
-              :type => 'update',
+              :type => 'flash',
               :source => 'migration',
               :body => status['label'],
             }
