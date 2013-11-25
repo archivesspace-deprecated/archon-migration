508d24e2ef8a9e436c379aedd200a6e6f214caa4
Brian Hoffman <brianjhoffman@gmail.com>
2013-11-25 13:51:20 -0500

    more rube goldberg order sorting
    fix previous commit

--- app/models/archon_content.rb
+++ app/models/archon_content.rb
@@ -207,6 +207,7 @@ Archon.record_type(:content) do
 
   def self.figure_out_position(rec, position=nil, xtra = [])
     position = rec['SortOrder'] unless position
+    position = pad(position, 3)
     parent_id = rec['ParentID']
 
     return position.to_i if  parent_id == '0' || parent_id.nil?
@@ -223,10 +224,11 @@ Archon.record_type(:content) do
         position = "#{position}#{pad(xtra.shift, 4)}" 
       end
 
-      # normalize all integers to 13 decimals
+      # normalize all integers to 16 decimal space
       # and assume no not-merely-physical node
       # will have 3 physical-only ancestors in a row
-      position = pad(position, 13, :right) 
+      # (3 x 4) + 4 = 16
+      position = pad(position, 16, :right) 
     
       return position.to_i
     end
