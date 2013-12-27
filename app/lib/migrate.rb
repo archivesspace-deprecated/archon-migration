02c236b36b38b5eae7d677f6f6e7eaa990ddb8ee
Brian Hoffman <brianjhoffman@gmail.com>
2013-12-27 15:16:22 -0500

    use rubyzip instead of zip

--- app/lib/migrate.rb
+++ app/lib/migrate.rb
@@ -2,7 +2,7 @@ require_relative 'startup'
 require_relative 'archon_client'
 require_relative 'archivesspace_client'
 require_relative 'migration_helpers'
-require 'zip/zip'
+require 'zip'
 
 
 class MigrationJob
@@ -26,8 +26,6 @@ class MigrationJob
 
     # 1 job per thread
     raise "Job thread occupied." if Thread.current[:archon_migration_job]
-    Thread.current[:archon_migration_job] = self
-
 
     @aspace = ArchivesSpace::Client.new(
                                         :url => @args[:aspace_url],
@@ -53,6 +51,7 @@ class MigrationJob
       FileUtils.rm_rf(download_path)
     end
 
+    Thread.current[:archon_migration_job] = self
   end
 
 
@@ -503,7 +502,7 @@ to an Agent. The matching Archon ID for the #{obj.jsonmodel_type} record is
     directory = Dir.tmpdir + "/archon_bitstreams/"
     zipfile_name = File.join(File.dirname(__FILE__), '../', 'public', 'bitstreams.zip')
    
-    Zip::ZipFile.open(zipfile_name, Zip::ZipFile::CREATE) do |zipfile|
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
       Dir.glob("#{directory}*.*").each do |file|
         zipfile.add(file.sub(directory, ''), file)
       end
